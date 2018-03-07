598d63bf48070190ed67932591d31e7ad30c372a
pparonson <preston.aronson@gmail.com>
2018-03-07 08:55:51 -0700

    add ramda refactoring

--- src/block.js
+++ src/block.js
@@ -1,23 +1,21 @@
 const R = require("ramda");
 const SHA256 = require("crypto-js/sha256");
 
-const createBlock = (_data, _previousHash) => {
+const createBlock = R.curry((data, previousHash) => {
   let timestamp = Date.now();
-  let data = _data;
-  let previousHash = _previousHash;
-  let hash = calculateHash(timestamp, data, previousHash); 
+  let hash = calculateHash(timestamp, data, previousHash);
+  
   return {
-    timestamp,
-    data,
+    timestamp, 
+    data, 
     previousHash,
     hash
-  }
-;}
+  };
+});
 
 // impure: sha256 causes side effects?
-const calculateHash = (timestamp, data, previousHash) => {
-  return SHA256(`${timestamp}${data}${previousHash}`).toString();
-};
+const calculateHash = R.curry((timestamp, data, previousHash) => 
+  SHA256(`${timestamp}${data}${previousHash}`).toString());
 
 module.exports = {
   createBlock
